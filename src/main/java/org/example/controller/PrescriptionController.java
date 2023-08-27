f42c568c0ad94f196c8f8eedef8cc6911667daca
CACTUS <mat.zaharoff@yandex.ru>
2023-08-27 22:23:35 +0300

    added ability to communicate with PDF-generator microservice

--- src/main/java/org/example/controller/PrescriptionController.java
+++ src/main/java/org/example/controller/PrescriptionController.java
@@ -6,10 +6,19 @@ import org.example.service.PatientService;
 import org.example.service.PrescriptionService;
 import org.example.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -56,10 +65,37 @@ public class PrescriptionController {
     public String returnPatientPrescription(Model model) {
         User currentUser = userService.getCurrentUser();
         Patient patient = patientService.findByUser(userService.getCurrentUser().getId());
+        Doctor doctor = patient.getDoctor();
         List<Prescription> prescriptions = patient.getPrescriptions();
         model.addAttribute("currentUser", currentUser);
+        model.addAttribute("patient_name", patient.getLastName() + ' ' + patient.getFirstName() + ' ' + patient.getMiddleName());
+        model.addAttribute("doctor_name", doctor.getLastName() + ' ' + doctor.getFirstName() + ' ' + doctor.getMiddleName());
         if (prescriptions.isEmpty()) return "patient/no_prescription.html";
         model.addAttribute("prescriptions", prescriptions);
         return "patient/prescription";
+    }
+
+    @Controller
+    @RequestMapping("/api/prescription/pdf")
+    public class TestPDFcontroller {
+        @GetMapping
+        public ResponseEntity<byte[]> ping(@RequestParam String patient_name, @RequestParam String doctor_name, @RequestParam String prescription_body, HttpServletResponse response) throws IOException {
+            final String uri = "http://localhost:8090/api/prescription" + "?patient_name=" + patient_name + "&doctor_name=" + doctor_name + "&prescription_body=" + prescription_body;
+            RestTemplate restTemplate = new RestTemplate();
+            byte[] bytes = restTemplate.getForObject(uri, byte[].class);
+            ByteArrayOutputStream baos_doc = new ByteArrayOutputStream();
+            baos_doc.writeBytes(bytes);
+
+            response.setContentType("application/pdf");
+            DateFormat dateFormat = new SimpleDateFormat("YYYY-MM-DD:HH:MM:SS");
+            String currentDateTime = dateFormat.format(new Date());
+            String headerkey = "Content-Disposition";
+            String headervalue = "attachment; filename=pdf_" + currentDateTime + ".pdf";
+            response.setHeader(headerkey, headervalue);
+
+            return ResponseEntity.ok()
+                    .contentType(MediaType.APPLICATION_PDF)
+                    .body(bytes);
+        }
     }
 }
